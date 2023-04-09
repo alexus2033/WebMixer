832004e2a5c3db7e288076dfe56576e17c347f2d
alexus2033 <85680663+alexus2033@users.noreply.github.com>
2023-04-09 20:26:20 +0000

    supercool first commit

---
+++ js/alphabeatmidiapi.js
@@ -0,0 +1,153 @@
+{
+	var inputs = [];
+	var inputDev = null;
+	var outputs = [];
+	var outputDev = null;
+
+	function runTest()
+	{
+		if (!log)
+		log = document.getElementById("log");
+
+		if (typeof navigator.requestMIDIAccess !== "function") {
+			printInfo("MIDI not available");
+			return; 
+		}	
+
+		navigator.permissions.query({ name: "midi", sysex: true }).then((result) => {
+			if (result.state === "granted") {
+				printInfo("Access granted.");
+			} else if (result.state === "prompt") {
+				printInfo("Please confirm permission request");
+			} else {
+				printInfo(result.state);
+			}
+		});
+		
+		navigator.requestMIDIAccess( { sysex: true } ).then( access, failure );
+	}
+
+	function access(midiAccess)
+	{
+		clearInfo();
+		assignInputsAndOutputs(midiAccess);
+		setDefault();
+
+		midiAccess.onstatechange = (event) => {
+			console.log("onstatechange");
+			assignInputsAndOutputs(midiAccess);
+			setDefault();
+		};
+	}
+
+	function setDefault(){
+		inputDev = null;
+		outputDev = null;
+		if(inputs.length > 0){
+			inputDev = inputs[0];
+			inputDev.onmidimessage = handleInputMessage;
+			//inputDev.onmidimessage = (msg) => {
+			//	console.log(msg.data);
+			//	handleMIDIMessage;
+			//};
+			printInfo(`${inputDev.state} ${inputDev.type}: ${inputDev.name}`);
+		}
+		if(outputs.length > 0){
+			outputDev = outputs[0];
+			printInfo(`${outputDev.state} ${outputDev.type}: ${outputDev.name}`);
+		}
+	}
+
+	function printInfo(value){
+		value += "\n";
+		if(log!=null){
+			log.innerText += value;
+		} else {
+			console.log(value);
+		}
+	}
+
+	function clearInfo(){
+		if(log!=null){
+			log.innerText = "";
+		}
+	}
+
+	function failure(msg)
+	{
+		printInfo(`Failed to get MIDI access - ${msg}`);
+	}
+
+	function assignInputsAndOutputs(midiAccess) {
+		if (typeof midiAccess.inputs === "function") {
+			inputs=midiAccess.inputs();
+			outputs=midiAccess.outputs();
+		} else {
+			var inputIterator = midiAccess.inputs.values();
+			inputs = [];
+			for (var o = inputIterator.next(); !o.done; o = inputIterator.next()) {
+				inputs.push(o.value)
+			}
+	
+			var outputIterator = midiAccess.outputs.values();
+			outputs = [];
+			for (var o = outputIterator.next(); !o.done; o = outputIterator.next()) {
+				outputs.push(o.value)
+			}
+		}
+	}
+	
+	function sendShortMsg(midiMessage) {
+		//omitting the timestamp means send immediately.
+		if(outputDev){
+			outputDev.send(midiMessage); 
+		}
+	}
+
+	function handleInputMessage( event ) {
+		var str=null;
+		if( event.data.length < 3)
+			return;
+
+		//handle Buttons
+		if(event.data[0] == 0x80 && event.data[2] == 0x40){
+			if(event.data[1] == 0x32){
+				pausePlay(0);
+			}
+			if(event.data[1] == 0x33){
+				pausePlay(1);
+			}
+			if(event.data[1] == 0x37){
+				player[0].currentTime = 0;
+			}
+			if(event.data[1] == 0x38){
+				player[1].currentTime = 0;
+			}
+			return;
+		}
+		//handle Fader
+		if(event.data[0] == 0xb0){
+			if(event.data[1] == 0x1f){
+				player[0].volume = event.data[2]/128;
+			}
+			if(event.data[1] == 0x29){
+				player[1].volume = event.data[2]/128;
+			}
+			return;
+		}
+
+		str ="data.length=" +event.data.length+ ":"+ " 0x" + event.data[0].toString(16) + ":";
+		if(log!=null) log.innerText += str;
+
+		for(var i=1,k=0; i<event.data.length; i++, k++){
+			str =" 0x" + event.data[i].toString(16) + ":";
+			if(log!=null) log.innerText += str;
+			if(i%8==0){
+				if(log!=null) log.innerText +="\n";
+			}
+		}
+		str ="\n"; 
+		if(log!=null) log.innerText += str;
+	}
+}
+
---
+++ js/player.js
@@ -0,0 +1,95 @@
+
+{
+    const msgEOM = 0x3a;
+    const playLED = 0x01;
+    var eom = [false,false];
+
+    function playerInactive(n){
+		if(!player[n].currentTime){
+		  return true;
+		}
+		return player[n].paused;
+	}
+
+    function loadTitle(file){
+        var url = URL.createObjectURL(file);
+        var id = 0;
+        if(!playerInactive(0)){
+          id = 1;
+        }
+        player[id].setAttribute('src', url);
+        player[id].load();
+        info[id].innerText = file.name;
+    }
+
+    // start and stop player
+	function pausePlay(id){
+		if(playerInactive(id)){
+			player[id].play();
+			sendShortMsg([0x90,playLED+id,0x7f]);
+		} else {
+			player[id].pause();
+			sendShortMsg([0x90,playLED+id,0x01]);
+		}
+	}
+
+    function updatePlayerTime(player, id){
+        // Update Time-Displays
+        player.addEventListener("timeupdate", (event) => {
+            var remain = player.duration - player.currentTime;
+            var mins = parseInt((remain/60)%60),
+            secs = parseInt(remain%60),
+            millis = remain.toFixed(2).slice(-2,-1);
+            sendShortMsg([0x94+id, 0x16, millis]);
+            if(prevSecs != secs){
+                sendShortMsg([0x94+id, 0x15, secs]);
+                prevSecs = secs;
+            }
+            if(prevMins != mins){
+                sendShortMsg([0x94+id, 0x14, mins]);
+                prevMins = mins;
+            }
+            if(remain < 21 && remain > 0 && eom[id] == false){
+                sendShortMsg([0x90,msgEOM+id,0x7f]);
+                eom[id] = true;
+            }
+            if(mins && outputs.length == 0){
+                pos[id].innerHTML = `${mins}:${secs.pad(2)}.${millis}`;
+            }
+        });
+    }
+
+    function startStopPlayer(player, id){
+        // Player started
+        player.addEventListener("play", (event) => {
+        sendShortMsg([0x90,0x01+id,0x7f]);
+        })
+        // Player stopped
+        player.addEventListener("pause", (event) => {
+        sendShortMsg([0x90,0x01+id,0x01]);
+        })
+    }
+
+    function playlistEnded(player, id){
+        player.addEventListener("ended", (event) => {
+            sendShortMsg([0x90,playLED+id,0x01]);
+            if(eom[id]==true){
+                sendShortMsg([0x90,msgEOM+id,0x01]);
+                eom[id] = false;
+            }
+            _next += 1;
+            next(_next);
+            console.log(len, _next);
+            if((len-1)==_next){
+              _next=-1;
+            }
+        });
+    }
+
+    // format number
+    Number.prototype.pad = function(size) {
+		var s = String(this);
+		while (s.length < (size || 2)) {s = "0" + s;}
+		return s;
+	}
+}
